19059c9a369a256fc6849424fc80bda1ab575dd2
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-06-14 18:34:23 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 import './mobile-menu';
+
+const contentBlock = document.querySelector('.content-block');
+
+console.log([...contentBlock.children].length);
+
+function even() {
+  [...contentBlock.children];
+}
+
+even();
