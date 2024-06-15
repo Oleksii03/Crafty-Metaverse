21109a5dc18ab772261acf04d587b36470fee915
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-06-15 18:48:22 +0300

    add

--- src/js/roadmap.js
+++ src/js/roadmap.js
@@ -1,4 +1,6 @@
 const contentBlock = document.querySelector('.content-block');
+const btnMore = document.querySelector('.js-list-more-btn');
+const innerList = document.querySelector('.js-list');
 
 (function () {
   [...contentBlock.children].forEach((el, idx) => {
@@ -7,3 +9,16 @@ const contentBlock = document.querySelector('.content-block');
     }
   });
 })();
+
+btnMore.addEventListener('click', e => {
+  innerList.classList.toggle('active');
+
+  if (innerList.classList.contains('active')) {
+    innerList.style.maxHeight = innerList.scrollHeight + 'px';
+    e.currentTarget.firstElementChild.textContent = 'Height ';
+    return;
+  }
+
+  innerList.style.maxHeight = 0;
+  e.currentTarget.firstElementChild.textContent = 'More';
+});
