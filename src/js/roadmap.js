ef9138cb650cc7adc68467a957c1908371054d81
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-06-16 14:40:47 +0300

    add-js

--- src/js/roadmap.js
+++ src/js/roadmap.js
@@ -1,6 +1,12 @@
 const contentBlock = document.querySelector('.content-block');
-const btnMore = document.querySelector('.js-list-more-btn');
-const innerList = document.querySelector('.js-list');
+
+const btnMoreQ1 = document.querySelector('.js-list-more-btn');
+const btnMoreQ2 = document.querySelector('.js-list-more-btn-q2');
+
+const innerListQ1 = document.querySelector('.js-list');
+const innerListQ2 = document.querySelector('.js-list-q2');
+
+const imgQ1 = document.querySelector('.js-bg-circle-q1');
 
 (function () {
   [...contentBlock.children].forEach((el, idx) => {
@@ -10,15 +16,41 @@ const innerList = document.querySelector('.js-list');
   });
 })();
 
-btnMore.addEventListener('click', e => {
-  innerList.classList.toggle('active');
+btnMoreQ1.addEventListener('click', onToggleInnerListQ1);
+btnMoreQ2.addEventListener('click', onToggleInnerListQ2);
 
-  if (innerList.classList.contains('active')) {
-    innerList.style.maxHeight = innerList.scrollHeight + 'px';
-    e.currentTarget.firstElementChild.textContent = 'Height ';
+function onToggleInnerListQ1() {
+  if (innerListQ2.classList.contains('active')) {
+    onToggleInnerListQ2();
+  }
+
+  if (!innerListQ1.classList.contains('active')) {
+    innerListQ1.classList.add('active');
+
+    innerListQ1.style.maxHeight = innerListQ1.scrollHeight + 'px';
+    btnMoreQ1.firstElementChild.textContent = 'Hide';
     return;
   }
 
-  innerList.style.maxHeight = 0;
-  e.currentTarget.firstElementChild.textContent = 'More';
-});
+  innerListQ1.style.maxHeight = 0;
+  btnMoreQ1.firstElementChild.textContent = 'More';
+  innerListQ1.classList.remove('active');
+}
+
+function onToggleInnerListQ2() {
+  if (innerListQ1.classList.contains('active')) {
+    onToggleInnerListQ1();
+  }
+  if (!innerListQ2.classList.contains('active')) {
+    innerListQ2.classList.add('active');
+
+    innerListQ2.style.maxHeight = innerListQ2.scrollHeight + 'px';
+
+    btnMoreQ2.firstElementChild.textContent = 'Hide';
+    return;
+  }
+
+  innerListQ2.style.maxHeight = 0;
+  btnMoreQ2.firstElementChild.textContent = 'More';
+  innerListQ2.classList.remove('active');
+}
